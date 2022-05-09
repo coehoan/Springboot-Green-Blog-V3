b63b1952f712aef89ccfbf1ade976959ceff76b6
coehoan <coehoan@naver.com>
2022-05-09 11:14:41 +0900

    좋아요, 좋아요 취소 UI 구현완료 1/2

--- src/main/resources/static/js/post/detail.js
+++ src/main/resources/static/js/post/detail.js
@@ -1,3 +1,27 @@
+function postLikeClick(id) {
+    let isLike = ($(`#${id}`).hasClass("my_fake_unlike"));
+    if (isLike) {
+        postLike(id);
+    } else {
+        postUnLike(id);
+    }
+}
+
+// 좋아요 누르기
+function postLike(id) {
+    $(`#${id}`).addClass("my_fake_like");
+    $(`#${id}`).removeClass("my_fake_unlike");
+    $(`#${id}`).addClass("fa-solid");
+    $(`#${id}`).removeClass("far");
+}
+// 좋아요 취소하기
+function postUnLike(id) {
+    $(`#${id}`).removeClass("my_fake_like");
+    $(`#${id}`).addClass("my_fake_unlike");
+    $(`#${id}`).removeClass("fa-solid");
+    $(`#${id}`).addClass("far");
+}
+
 // 게시글 삭제, 권한체크 후 삭제 /s/api/post/postId
 $("#btn-delete").click(() => {
     postDelete();
